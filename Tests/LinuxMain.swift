e45c101d4224365d239a657451c6c61e8528e70d
Richard Piazza <richard@richardpiazza.com>
2020-08-22 10:34:26 -0500

    Added 'LinuxMain' for testing infrastructure.

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,13 @@
+import XCTest
+
+import ProcedureKitTests
+import ProcedureKitStressTests
+import ProcedureKitCloudTests
+import ProcedureKitCoreDataTests
+import ProcedureKitLocationTests
+import ProcedureKitMacTests
+import ProcedureKitMobileTests
+import ProcedureKitNetworkTests
+
+var tests = [XCTestCaseEntry]()
+XCTMain(tests)
